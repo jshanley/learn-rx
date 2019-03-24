57babbd131ec2b1bf93dba94d81c7cd7d27e29df
John Shanley <john.michael.shanley@gmail.com>
2019-03-24 17:21:28 -0400

    Use stream to log keypress events

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+
+function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    const input$ = fromEvent(document.body, 'keypress');
+    input$.pipe(map(evt => evt.key));
+    const subscription = input$.subscribe(value => console.log(value));
+
+    return () => subscription.unsubscribe();
+
+  }, [])
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
 }
 
 export default App;
